9dc64ffac176794d2342fcc3e4da377290627b04
Giovanna Barra <giovanna@mihost.com>
2022-04-24 22:51:12 -0600

    client and server connected

--- web_server/src/bin/main.rs
+++ web_server/src/bin/main.rs
@@ -2,24 +2,22 @@ mod prethread;
 mod http_client;
 
 fn main() {
-    //while true {
+    loop {
         let mut input = String::new();
         println!("Enter command (write exit to end): ");
-        let mut input_bytes = std::io::stdin().read_line(&mut input).unwrap();
+        let mut _input_bytes = std::io::stdin().read_line(&mut input).unwrap();
         if input.starts_with("prethread-WebServer"){
-            let mut input = input.trim(); //removes \n from read_line
+            input = input.trim().to_string(); //removes \n from read_line
             let input_vector: Vec<String> = input.split(' ').map(|s| s.to_string()).collect(); // creates a vector from args
-            println!("vector: {:?}", input_vector);
             prethread::init(input_vector);
             // preforked-WebServer -n cantidad -w http-root -p port
         } else if input.starts_with("preforked-WebServer"){
             // code for preforked
             // preforked-WebServer -n cantidad -w http-root -p port
         } else if input.starts_with("HTTPclient"){
-            let mut input = input.trim(); //removes \n from read_line
+            input = input.trim().to_string(); //removes \n from read_line
             let input_vector: Vec<String> = input.split(' ').map(|s| s.to_string()).collect(); // creates a vector from args
-            println!("vector: {:?}", input_vector);
-            http_client::init(input_vector);
+            prethread::client_init(input_vector);
             // HTTPCLient -h <host a conextar> [lista de comandos a ejecutar]
         } else if input.starts_with("stress"){
             // code for stress cmd
@@ -28,5 +26,5 @@ fn main() {
         } else {
             println!("Invalid command");
         }
-    //}
+    }
 }
--- web_server/src/bin/prethread.rs
+++ web_server/src/bin/prethread.rs
@@ -6,31 +6,9 @@ use std::fs;
 use std::time::Duration;
 use web_server::ThreadPool;
 use std::thread;
-use ftp::FtpStream;
+use std::process::Command;
 
-/*
-struct Database {
-    data: Vec<json::JsonValue>
-}
-
-impl Database{
-    fn init() -> Database {
-        let data = Vec::new();
-        let info = json::object!{
-            title: "test.txt",
-            contents: fs::read_to_string("test.txt").unwrap()
-        };
-        data.push(info)
-        
-        Database { data }
-    }
-
-    fn push(value: json:JsonValue) {
-        
-    }
-}*/
-static mut database: Vec<serde_json::Value> = Vec::new();
-//static mut database: Vec<json::JsonValue> = Vec::new();
+static mut DATABASE: Vec<serde_json::Value> = Vec::new();
 
 pub fn init(args: Vec<String>) {
     let mut max = args.len() - 1;
@@ -64,9 +42,7 @@ pub fn init(args: Vec<String>) {
 fn server_init(root: &str, port: &str, n: usize) {
     let http_url = format!("{}:{}", root, port);
     println!("{}", http_url);
-    //let mut ftp_stream = FtpStream::connect(http_url).unwrap_or_else(|err|
-    //    panic!("{}", err)
-    //);
+
     let listener = TcpListener::bind(http_url).unwrap();        // returns a new tcp listener, returns Result<T, E>
 
     let pool = ThreadPool::new(n);
@@ -80,38 +56,80 @@ fn server_init(root: &str, port: &str, n: usize) {
     }
 }
 
+pub fn client_init(args: Vec<String>) {
+    let mut max = args.len() - 1;
+    let mut counter = 1;
+    let mut host_to_connect: &str = "localhost:7878/";
+
+    while max > 0 {
+        let flag = &args[counter];
+        let flag_value = &args[counter + 1];
+        
+        if flag == "-h" {
+            host_to_connect = flag_value;
+        } else if flag.starts_with("["){
+            let comm_list = &args[counter..];//.join(" ");
+            format_command(comm_list.to_vec(), host_to_connect);
+
+            break;
+        } else {
+            println!("ERROR: invalid flag. Valid options: -h");
+            break;
+        }
+        max -= 2;
+        counter += 2;
+    }
+
+}
+
+fn format_command(mut commands: Vec<String>, host: &str) {
+    // HTTPclient [-X PUT -d @test.txt]
+    let last = commands.len() - 1;
+    commands[0] = commands[0].trim_start_matches("[").to_string();
+    commands[last] = commands[last].trim_end_matches("]").to_string();
+    let url = format!("{}{}", host.to_string(), commands[last].trim_start_matches("@"));
+    commands.insert(0, url);
+
+
+    Command::new("curl")
+            .args(&commands)
+            .output()
+            .unwrap();
+    
+}
+
 fn handle_requests(mut stream: TcpStream){
     let mut buffer = [0; 1024];         // holds the data as its read on, 1024 bytes
     stream.read(&mut buffer).unwrap();
 
-    //let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
     let buffer_vector: Vec<String> = std::str::from_utf8(&buffer).unwrap().split(' ').map(|s| s.to_string()).collect(); // creates a vector from args
+    println!("{:?}", std::str::from_utf8(&buffer));
 
     if buffer.starts_with(sleep) {
         thread::sleep(Duration::from_secs(5));
-        get("HTTP/1.1 200 OK", "hi.html", stream);
+        get("HTTP/1.1 200 OK", stream);
     } else if buffer.starts_with(b"GET") || buffer.starts_with(b"POST") {
-        get("HTTP/1.1 200 OK", "hi.html", stream);
+        get("HTTP/1.1 200 OK", stream);
     } else if buffer.starts_with(b"PUT") {
-        put("HTTP/1.1 200 OK", buffer_vector[1].trim_start_matches("/"), stream); // /bin/zip ERR stream did not contain valid UTF-8
+        put("HTTP/1.1 200 OK", buffer_vector[1].trim_start_matches("/"), stream);
     } else if buffer.starts_with(b"DELETE") {
-        put("HTTP/1.1 200 OK", "test.txt", stream);
+        //put("HTTP/1.1 200 OK", "test.txt", stream);
     } else if buffer.starts_with(b"HEAD") {
-        put("HTTP/1.1 200 OK", "hi.html", stream);
+        //put("HTTP/1.1 200 OK", "hi.html", stream);
     } else {
         println!("ERROR: Invalid command.");
     }
 }
 
 
-fn get(status_line: &str, filename: &str, mut stream: TcpStream){
+fn get(status_line: &str, mut stream: TcpStream){
     unsafe {
         let response = format!(
             "{}\r\nContent-Length: {}\r\n\r\n{:#?}",
             status_line,
-            database.len(),
-            database
+            DATABASE.len(),
+            DATABASE
         );
 
         match stream.write(response.as_bytes()) {
@@ -122,10 +140,11 @@ fn get(status_line: &str, filename: &str, mut stream: TcpStream){
     }
 }
 
-fn put(status_line: &str, filename: &str, mut stream: TcpStream) {
+fn put(status_line: &str, file: &str, mut stream: TcpStream) {
+    println!("ARCHIVO: {}", file);
     let data = serde_json::json!({
-        "title": filename,
-        "contents": fs::read_to_string(filename).unwrap()
+        "title": file,
+        "contents": fs::read_to_string(file).unwrap()
     });
 
     let response = format!(
@@ -136,7 +155,7 @@ fn put(status_line: &str, filename: &str, mut stream: TcpStream) {
     );
 
     unsafe {
-        database.push(data);
+        DATABASE.push(data);
     }
 
     match stream.write(response.as_bytes()) {

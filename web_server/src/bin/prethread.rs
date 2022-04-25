1cac421f5970543da17501f072fa2a969ab718f1
Giovanna Barra <giovanna@mihost.com>
2022-04-24 23:48:02 -0600

    http methods

--- web_server/src/bin/prethread.rs
+++ web_server/src/bin/prethread.rs
@@ -68,8 +68,9 @@ pub fn client_init(args: Vec<String>) {
         if flag == "-h" {
             host_to_connect = flag_value;
         } else if flag.starts_with("["){
-            let comm_list = &args[counter..];//.join(" ");
-            format_command(comm_list.to_vec(), host_to_connect);
+            let comm_list = &args[counter..];
+            let method = &args[counter + 1].trim_end_matches("]").to_string();
+            format_command(comm_list.to_vec(), host_to_connect, method);
 
             break;
         } else {
@@ -82,19 +83,38 @@ pub fn client_init(args: Vec<String>) {
 
 }
 
-fn format_command(mut commands: Vec<String>, host: &str) {
+fn format_command(mut commands: Vec<String>, host: &str, method: &str) {
     // HTTPclient [-X PUT -d @test.txt]
+    // HTTPclient [-X DELETE test.txt]
+    // HTTPclient [-X GET]
+    // HTTPclient [-X HEAD]
     let last = commands.len() - 1;
     commands[0] = commands[0].trim_start_matches("[").to_string();
     commands[last] = commands[last].trim_end_matches("]").to_string();
-    let url = format!("{}{}", host.to_string(), commands[last].trim_start_matches("@"));
-    commands.insert(0, url);
 
+    if method == "PUT" {
+        let url = format!("{}{}", host.to_string(), commands[last].trim_start_matches("@"));
+        commands.insert(0, url);
+    } else if method == "HEAD" {
+        commands = vec!["-I".to_string(), host.to_string()];
+    } else if method == "GET" || method == "POST"{
+        commands.insert(0, host.to_string());
+    } else if method == "DELETE" {
+        let url = format!("{}{}", host.to_string(), commands[last]);
+        commands.insert(last, url);
+    }
 
-    Command::new("curl")
-            .args(&commands)
-            .output()
-            .unwrap();
+    let output = Command::new("curl")
+                        .args(&commands)
+                        .output()
+                        .unwrap()
+                        .stdout;
+
+    let out = std::str::from_utf8(&output).unwrap(); //Vec<u8> to &str
+
+    for charac in out.chars(){
+        print!("{}", charac)
+    }
     
 }
 
@@ -104,7 +124,6 @@ fn handle_requests(mut stream: TcpStream){
 
     let sleep = b"GET /sleep HTTP/1.1\r\n";
     let buffer_vector: Vec<String> = std::str::from_utf8(&buffer).unwrap().split(' ').map(|s| s.to_string()).collect(); // creates a vector from args
-    println!("{:?}", std::str::from_utf8(&buffer));
 
     if buffer.starts_with(sleep) {
         thread::sleep(Duration::from_secs(5));
@@ -114,9 +133,9 @@ fn handle_requests(mut stream: TcpStream){
     } else if buffer.starts_with(b"PUT") {
         put("HTTP/1.1 200 OK", buffer_vector[1].trim_start_matches("/"), stream);
     } else if buffer.starts_with(b"DELETE") {
-        //put("HTTP/1.1 200 OK", "test.txt", stream);
+        delete("HTTP/1.1 200 OK", buffer_vector[1].trim_start_matches("/"), stream);
     } else if buffer.starts_with(b"HEAD") {
-        //put("HTTP/1.1 200 OK", "hi.html", stream);
+        head("HTTP/1.1 200 OK", stream);
     } else {
         println!("ERROR: Invalid command.");
     }
@@ -141,7 +160,6 @@ fn get(status_line: &str, mut stream: TcpStream){
 }
 
 fn put(status_line: &str, file: &str, mut stream: TcpStream) {
-    println!("ARCHIVO: {}", file);
     let data = serde_json::json!({
         "title": file,
         "contents": fs::read_to_string(file).unwrap()
@@ -163,4 +181,42 @@ fn put(status_line: &str, file: &str, mut stream: TcpStream) {
         Err(e) => println!("ERROR: {}", e),
     }
     stream.flush().unwrap();
+}
+
+
+fn delete(status_line: &str, file: &str, mut stream: TcpStream) {
+    unsafe {
+        for i in 0..DATABASE.len() {
+            println!("{}", DATABASE[i]["title"]);
+            if DATABASE[i]["title"] == file {
+                DATABASE.remove(i);
+            }
+        }
+    
+
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{:#?}",
+            status_line,
+            DATABASE.len(),
+            DATABASE
+        );
+
+        match stream.write(response.as_bytes()) {
+            Ok(_) => println!("Response: {}", response),
+            Err(e) => println!("ERROR: {}", e),
+        }
+        stream.flush().unwrap();
+    }
+}
+
+fn head(status_line: &str, mut stream: TcpStream){
+    unsafe {
+        let response = format!("{}\r\n", status_line);
+
+        match stream.write(response.as_bytes()) {
+            Ok(_) => println!("Response: {}", response),
+            Err(e) => println!("ERROR: {}", e),
+        }
+        stream.flush().unwrap();
+    }
 }
